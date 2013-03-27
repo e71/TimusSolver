172c9812251ba342f8d827619813d60b6748b663
Viacheslav Rostovtsev <vrostovtsev@gmail.com>
2013-03-27 09:05:13 +0400

    Initial commit

---
+++ TimusSolution/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimusSolution
+{
+    namespace P1005
+    {
+        public class P1005Solver
+        {
+            public string Solve(int[] stones)
+            {
+                return String.Empty;
+            }
+
+            public int[] ReadInput(System.IO.TextReader inStream)
+            {
+                var nfi = NumberFormatInfo.InvariantInfo;
+                var args = inStream.ReadToEnd().Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
+
+                var lenstr = Int32.Parse(args[0], nfi);
+                if (lenstr ==0)
+                    return new int[0];
+                var entries = args[1].Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Take(lenstr).Select(entr => Int32.Parse(entr, nfi)).ToArray();
+
+                return entries;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new TimusSolution.P1005.P1005Solver();
+            var input = solver.ReadInput(Console.In);
+            var output = solver.Solve(input);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ TimusTest/P1005/P1005Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TimusTest.P1005
+{
+    class P1005Tests
+    {
+        [Fact]
+        public void VerifySolverReturnsEmptyArrayOn0()
+        {
+            var solver = new TimusSolution.P1005.P1005Solver();
+            var inputStream = new System.IO.StringReader(String.Format("{1}{0}{2}", Environment.NewLine, "0", String.Empty));
+            var entries = solver.ReadInput(inputStream);
+
+            Assert.Equal(0, entries.Length);
+        }
+
+        [Fact]
+        public void VerifySolverParsesSingleElemssCorrectly()
+        {
+            var solver = new TimusSolution.P1005.P1005Solver();
+            var inputStream = new System.IO.StringReader(String.Format("{1}{0}{2}", Environment.NewLine, "1", "5"));
+            var entries = solver.ReadInput(inputStream);
+            var expected = new int[] { 5 };
+
+            Assert.True(expected.SequenceEqual(entries));
+        }
+
+        [Fact]
+        public void VerifySolverParsesArraysCorrectly()
+        {
+            var solver = new TimusSolution.P1005.P1005Solver();
+            var inputStream = new System.IO.StringReader(String.Format("{1}{0}{2}", Environment.NewLine, "5", "5 8 13 27 14"));
+            var entries = solver.ReadInput(inputStream);
+            var expected = new int[] { 5, 8, 13, 27, 14 };
+
+            Assert.True(expected.SequenceEqual(entries));
+        }
+    }
+}
